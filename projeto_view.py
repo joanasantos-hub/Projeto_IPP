3e20cf306953e0f9efc020ccc0f7ee4ba2e4d1eb
joanasantos-hub <a103904@uminho.pt>
2025-04-05 13:52:37 +0100

    Update projeto_view.py

--- projeto_view.py
+++ projeto_view.py
@@ -3,7 +3,7 @@ import PySimpleGUI as sg
 import projeto_shell as logic
 import projeto_model as model
 
-sg.theme_background_color("#FFC0CB")  # Cor de fundo rosa claro
+sg.theme_background_color("#FFC0E1")  # Cor de fundo rosa claro
 sg.theme_button_color(("white", "#FF69B4"))  # Botões rosa escuro
 
 def criar_layout():
@@ -30,15 +30,15 @@ def Novo_Reg_Layout():
 
     layout = [
 
-        [sg.Text('Nome Completo:', font=("Arial Rounded Mt Bold", 12), text_color='white', background_color='#FF69B4', key= '-REG_NOME-'), sg.Input(key= '-NOME-')],
-        [sg.Text('Data de Nascimento: (Y-M-D)', font=("Arial Rounded Mt Bold", 12), text_color='white', background_color='#FF69B4', key='-REG_DOB-'), sg.Input(key= '-DOB-')],
-        [sg.Text('Sexo', font=("Arial Rounded Mt Bold", 12), text_color='white', background_color='#FF69B4', key='-REG_SEXO-'), sg.Combo(['M', 'F'], key= '-SEXO-')],
+        [sg.Text('Nome Completo *:', font=("Arial Rounded Mt Bold", 12), text_color='white', background_color='#FF69B4', key= '-REG_NOME-'), sg.Input(key= '-NOME-')],
+        [sg.Text('Data de Nascimento (A-M-D) *:', font=("Arial Rounded Mt Bold", 12), text_color='white', background_color='#FF69B4', key='-REG_DOB-'), sg.Input(key= '-DOB-')],
+        [sg.Text('Sexo *:', font=("Arial Rounded Mt Bold", 12), text_color='white', background_color='#FF69B4', key='-REG_SEXO-'), sg.Combo(['M', 'F'], key= '-SEXO-')],
         [sg.Text('Condições Prévias:', font=("Arial Rounded Mt Bold", 12), text_color='white', background_color='#FF69B4', key='-REG_COND-'), sg.Input(key= '-COND-')],
         [sg.Text('Medicações:', font=("Arial Rounded Mt Bold", 12), text_color='white', background_color='#FF69B4', key='-REG_MEDS-'), sg.Input(key= '-MEDS-')],
-        [sg.Text('Número de Cartão de Cidadão:', font=("Arial Rounded Mt Bold", 12), text_color='white', background_color='#FF69B4', key='-REG_CC-'), sg.Input(key= '-CC-')],
-        [sg.Text('NIF:', font=("Arial Rounded Mt Bold", 12), text_color='white', background_color='#FF69B4', key='-REG_NIF-'), sg.Input(key= '-NIF-')],
-        [sg.Text('Localidade:', font=("Arial Rounded Mt Bold", 12), text_color='white', background_color='#FF69B4', key='-REG_LOCAL-'), sg.Input(key= '-LOCAL-')],
-        [sg.Text('Contacto Pessoal:', font=("Arial Rounded Mt Bold", 12), text_color='white', background_color='#FF69B4', key='-REG_CP-'), sg.Input(key= '-CP-')],
+        [sg.Text('Número de Cartão de Cidadão *:', font=("Arial Rounded Mt Bold", 12), text_color='white', background_color='#FF69B4', key='-REG_CC-'), sg.Input(key= '-CC-')],
+        [sg.Text('NIF *:', font=("Arial Rounded Mt Bold", 12), text_color='white', background_color='#FF69B4', key='-REG_NIF-'), sg.Input(key= '-NIF-')],
+        [sg.Text('Localidade *:', font=("Arial Rounded Mt Bold", 12), text_color='white', background_color='#FF69B4', key='-REG_LOCAL-'), sg.Input(key= '-LOCAL-')],
+        [sg.Text('Contacto Pessoal *:', font=("Arial Rounded Mt Bold", 12), text_color='white', background_color='#FF69B4', key='-REG_CP-'), sg.Input(key= '-CP-')],
         [sg.Text('Parente mais Próximo:', font=("Arial Rounded Mt Bold", 12), text_color='white', background_color='#FF69B4', key='-REG_NOK-'), sg.Input(key= '-NOK-')],
         [sg.Text('Contacto do Parente mais Próximo:', font=("Arial Rounded Mt Bold", 12), text_color='white', background_color='#FF69B4', key='-REG_CNOK-'), sg.Input(key= '-CNOK-')],
         [sg.Button('Registar', font=("Arial Rounded Mt Bold", 12), key='-REGISTAR-'), sg.Button('Cancelar', font=("Arial Rounded Mt Bold", 12), key='-CANCELAR-')]
@@ -55,6 +55,10 @@ def Novo_Reg_Layout():
         
         elif event == '-REGISTAR-':
 
+            if (not values['-NOME-'] or not values['-DOB-'] or not values['-SEXO-'] or not values['-NIF-'] or not values['-CP-'] or not values['-LOCAL-']):
+                sg.popup('Por favor preencha os campos obrigatórios (*)!', font=("Arial Rounded Mt Bold", 14), text_color='white', background_color='#FFC0E1')
+                continue
+
             try:
                 paciente = logic.Paciente(
 
@@ -63,20 +67,51 @@ def Novo_Reg_Layout():
                     sexo = values['-SEXO-'],
                     cond_prévias = values['-COND-'],
                     medicações = values['-MEDS-'],
-                    CC = values['-CC-'],
-                    NIF = values['-NIF-'],
-                    contacto = values['-CP-'],
+                    CC = int(values['-CC-']) if values['-CC-'] else None,
+                    NIF = int(values['-NIF-']) if values['-NIF-'] else None,
+                    contacto = int(values['-CP-']) if values['-CP-'] else None,
                     NOK = values['-NOK-'],
-                    NOK_contacto = values['-CNOK-'],
+                    NOK_contacto = int(values['-CNOK-']) if values['-CNOK-'] else None,
                     localidade = values['-LOCAL-']
                 )
 
-                sg.popup('Paciente registado com sucesso!')
+                resultado = paciente.res
+                sg.popup(resultado, font=("Arial Rounded Mt Bold", 14), text_color='white', background_color='#FFC0E1')
 
             except:
-                sg.popup('Erro!  Não foi possível guardar o registo!')
+                sg.popup('Erro! Não foi possível guardar o registo!', font=("Arial Rounded Mt Bold", 14), text_color='white', background_color='#FFC0E1')
 
         window.close()
+
+def Log_In_Layout():
+
+    layout = [[sg.Text('Número de Cartão de Cidadão:', font=("Arial Rounded Mt Bold", 12), text_color='white', background_color='#FF69B4', key= '-LOG_CC-'), sg.Input(key= '-UTENTE_CC-')],
+              [sg.Button('Entrar', font=("Arial Rounded Mt Bold", 12), key='-ENTRAR-'), 
+               sg.Button('Cancelar', font=("Arial Rounded Mt Bold", 12), key='-CANCELAR-')]]
+
+    window = sg.Window('Log In de Utente', layout, modal=True) # Utilizamos uma janela modal de modo a que não seja possível a interação com a janela principal durante o processo de registo!
+
+    while True:
+        event, values = window.read()
+
+        if event in (sg.WIN_CLOSED, '-CANCELAR-'):
+            window.close()
+            return
+        
+        elif event == '-ENTRAR-':
+            try:
+                login = model.log_in(
+
+                    CC = int(values['-UTENTE_CC-'])
+                )
+            except:
+                sg.popup('Registo de utente não encontrado! Por favor realize a sua inscrição na plataforma', font=("Arial Rounded Mt Bold", 14), text_color='white', background_color='#FFC0E1')
+
+def Médicos_Esp_Layout():
+    pass
+
+def Campanha_Vac_Layout():
+    pass
 
 def main():
 
@@ -94,8 +129,18 @@ def main():
         # Novo Registo de Pacientes
         elif event == '-NOVO_REG-':
             Novo_Reg_Layout()
+        
+        elif event == '-UTENTE-':
+            Log_In_Layout()
+        
+        elif event == '-MD-':
+            Médicos_Esp_Layout()
+        
+        elif event == '-VAC-':
+            Campanha_Vac_Layout()
 
     window.close()
 
 if __name__ == "__main__":
     main()
+    window.close()
